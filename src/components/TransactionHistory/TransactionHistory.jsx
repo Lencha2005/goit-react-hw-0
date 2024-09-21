9d827b82998316dd3f084911e44d4cdc96e74782
Lena_Bondar <lencha.2005@gmail.com>
2024-09-21 14:03:59 +0300

    update

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -11,7 +11,6 @@ const TransactionHistory = ({items}) => {
       <th>Currency</th>
     </tr>
   </thead>
-
   <tbody>
     {items.map(item => {
     return (
@@ -23,7 +22,6 @@ const TransactionHistory = ({items}) => {
     )})}
   </tbody>
 </table>
-
   )
 }
 
